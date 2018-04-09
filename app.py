91088f6ff5e740d1506d49afb6406bb24dc0f76a
Yauyao <eric123987@gmail.com>
2018-04-09 23:42:29 +0800

    Revert "Update:Test Replay Message Change"
    This reverts commit a513049

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def handle_message(event):
     message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(
         event.reply_token,
-        "幹 吵屁")
+        message)
 
 import os
 if __name__ == "__main__":
