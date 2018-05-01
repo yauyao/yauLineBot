25964c4f164cb941e4afd1e229e88c9766b28632
Yauyao <eric123987@gmail.com>
2018-05-01 16:03:50 +0800

    Update:減少關鍵字的問題

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def callback():
 def handle_message(event):
     outInfo = ""
     print("on Call"+event.message.text)
-    if "機票" in event.message.text:
+    if "!機票" in event.message.text:
         resp = requests.get('https://www.ptt.cc/bbs/Japan_Travel/index.html')
         soup = BeautifulSoup(resp.text, 'html.parser')
         main_titles = soup.find_all('div', 'title')
